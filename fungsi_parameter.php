54ba61e59b8daadd4de2abcc67705020a9bda52e
saniyarahma <73781686+saniyarahma@users.noreply.github.com>
2020-11-29 19:34:24 +0700

    Add files via upload

---
+++ fungsi_parameter.php
@@ -0,0 +1,12 @@
+<?php
+    // Membuat Fungsi/Function dengan Parameter
+    function perkenalan($salam, $nama){
+        echo $salam." <br>";
+        echo "Selamat datang di channel ".$nama."<br>";
+        echo "Jangan lupa like, komen, dan subscribe ya<br>";
+    }
+
+    // Memanggil fungsi dengan parameter
+    perkenalan("Assalamualaikum", "Saniya");
+?>
+    

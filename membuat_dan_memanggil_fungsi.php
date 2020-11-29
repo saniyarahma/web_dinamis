0a706fcd0e9bca80011b1a9835a6360e606f5ea7
saniyarahma <73781686+saniyarahma@users.noreply.github.com>
2020-11-29 19:33:29 +0700

    Add files via upload

---
+++ membuat_dan_memanggil_fungsi.php
@@ -0,0 +1,11 @@
+<?php
+    //Memmbuat Fungsi/Function
+    function perkenalan(){
+        echo "Salam sejahtera<br>";
+        echo "Selamat datang di channel Saniya<br>";
+        echo "Jangan lupa like, komen, dan subscribe ya<br>";
+    }
+
+    // Memanggil fungsi
+    perkenalan();
+?>
